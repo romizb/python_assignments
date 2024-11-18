fc573c9dde9f2074009f6be7603a3b76c6cfd3a9
romizb <ruchamab123@gmail.com>
2024-11-18 15:54:53 +0200

    Add files via upload

---
+++ day03/circle_03.py
@@ -0,0 +1,15 @@
+# circle_03.py
+
+import math
+
+# Define the radius of the circle
+radius = int(input("enter radius: "))  # variable 1
+
+
+# Calculate area and circumference
+area = math.pi * radius ** 2
+circumference = 2 * math.pi * radius
+
+# Print results
+print("Area of the circle:", area)
+print("Circumference of the circle:", circumference)
---
+++ day03/rectangle_03.py
@@ -0,0 +1,14 @@
+# rectangle_03.py
+
+# Define width and length of the rectangle
+width = int(input("enter width: "))
+length = int(input("enter length: "))
+area = width * length
+
+# Calculate area and circumference
+area = width * length
+circumference = 2 * (width + length)
+
+# Print results
+print("Area of the rectangle:", area)
+print("Circumference of the rectangle:", circumference)
